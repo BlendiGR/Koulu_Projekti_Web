5cb9563b1ae83cd467d35fd590f2a361c5f6e3f8
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-11-24 21:52:11 +0200

    Implement authentication features: add login controller, current user retrieval, and validation

---
+++ server/src/api/validators/auth-validators.js
@@ -0,0 +1,9 @@
+import {body} from "express-validator";
+
+/**
+ * Validation chain for login.
+ */
+export const validateLogin = [
+    body("email").isEmail().withMessage("Must be a valid email address"),
+    body("password").isString().isLength({min: 8}),
+]
