c4cc988280d376f49398e75bb149412b0c363718
Temetski33 <teemu.laasio@gmail.com>
2025-12-09 16:28:19 +0200

    Add buttons to layout

--- client/src/features/menu/pages/Menu.jsx
+++ client/src/features/menu/pages/Menu.jsx
@@ -3,18 +3,58 @@ import ProductCard from "/src/components/common/ui/ProductCard.jsx";
 import { allProducts } from "/src/config/allProducts.js";
 import { mostBuyedProducts } from "/src/config/mostBuyedSection.js";
 
-
 const Menu = () => {
   const { t } = useLang();
+  const testloggg = () => {
+    console.log("Did it yeah");
+  };
   return (
     <div>
       <section className="p-8 mt-20 bg-beige text-center shadow-xl rounded-3xl">
-        <h1 className="md:text-5xl text-4xl mb-4 pt-11">
-          {t("menu.title")}
-        </h1>
+        <h1 className="md:text-5xl text-4xl mb-4 pt-11">{t("menu.title")}</h1>
         <p className="text-gray-700 max-w-xl mx-auto text-lg md:text-xl">
           {t("menu.subheading")}
         </p>
+
+        <div class="flex md:flex-row flex-col justify-center items-center gap-4">
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            All
+          </button>
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            Pizzas
+          </button>
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            Kebab
+          </button>
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            Burgers
+          </button>
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            Salads
+          </button>
+          <button
+            onClick={testloggg}
+            className="flex justify-center text-center text-white md:text-2xl text-xl font-semibold bg-red-100 py-3 w-full rounded-2xl cursor-pointer active:bg-red-200 hover:bg-red-200 mt-5"
+          >
+            Drinks
+          </button>
+        </div>
+
         <div className="flex md:flex-row flex-col justify-center items-center gap-10 mt-10">
           {allProducts.map((item) => (
             <ProductCard key={item.id} item={item} />
