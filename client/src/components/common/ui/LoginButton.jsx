16b55f62ccea3cdf7f8cda1c0baa0efabb84eeba
Blendi Grajqevci <blendi.gr@hotmail.fi>
2025-12-10 15:39:51 +0200

    feat: Implement a comprehensive navigation bar with mobile drawer and dynamic login/logout button.

--- client/src/components/common/ui/LoginButton.jsx
+++ client/src/components/common/ui/LoginButton.jsx
@@ -5,7 +5,7 @@ import { useLang } from "/src/hooks/useLang";
 const LoginButton = ({ user, onClick, mobile = false, buttonClass = "" }) => {
   const { t } = useLang();
   const baseClass =
-    "bg-black-200 text-white font-medium px-4 py-2.5 rounded-xl flex items-center justify-center gap-1 cursor-pointer";
+    "bg-black-200 active:bg-gray-200 text-white font-medium px-4 py-2.5 rounded-xl flex items-center justify-center gap-1 cursor-pointer";
 
   const className = [baseClass, !mobile && buttonClass]
     .filter(Boolean)
@@ -21,7 +21,7 @@ const LoginButton = ({ user, onClick, mobile = false, buttonClass = "" }) => {
   }
 
   return (
-    <NavLink to="/login" className={className}>
+    <NavLink to="/login" className={className} onClick={onClick}>
       <User />
       {t("nav.login")}
     </NavLink>
--- client/src/components/layout/Navbar/NavBar.jsx
+++ client/src/components/layout/Navbar/NavBar.jsx
@@ -31,26 +31,6 @@ const NavBar = ({ announcement, onDismiss }) => {
   const { user, handleLogout } = useAuth();
   const navLinks = getNavLinks(user, t);
 
-  // Estä scrollia kun mobile menu on auki
-  /*useEffect(() => {
-    if (mobileMenuOpen || cartDrawerOpen) {
-      const scrollY = window.scrollY;
-      
-      document.body.style.position = 'fixed';
-      document.body.style.top = `-${scrollY}px`;
-      document.body.style.width = '100%';
-      document.body.style.overflow = 'hidden';
-      
-      return () => {
-        document.body.style.position = '';
-        document.body.style.top = '';
-        document.body.style.width = '';
-        document.body.style.overflow = '';
-        window.scrollTo(0, scrollY);
-      };
-    }
-  }, [mobileMenuOpen, cartDrawerOpen]);*/
-
   return (
     <>
       <nav className={navClass}>
