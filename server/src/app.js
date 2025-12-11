9528d4368bf9373f2a2557496904de0772d83874
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-12-11 13:48:19 +0200

    Add support for public/ client serving to backend app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import api from "./api/index.js";
 import {errorHandler, notFoundHandler} from "./middleware/error-handlers.js";
 import {successResponse} from "./middleware/success-response.js";
 import cors from "cors";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Static Files
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
 // Body Parsers
@@ -25,6 +30,11 @@ app.use(successResponse);
 
 // API Routes
 app.use("/api/v1", api);
+
+app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 // Error Handlers
 app.use(notFoundHandler);
